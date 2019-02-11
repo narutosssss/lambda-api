2c0999f98da7a1804b51e8f93e2370d5118bfc35
Kritsada Sirirangsi <kritsada.s@orisma.com>
2019-02-11 18:21:50 +0700

    no message

--- modules/api/main.tf
+++ modules/api/main.tf
@@ -78,8 +78,9 @@ resource "aws_lambda_permission" "allow_api_gateway" {
   statement_id  = "AllowExecutionFromApiGateway"
   action        = "lambda:InvokeFunction"
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:${local.region}:${local.account_id}:${aws_api_gateway_rest_api.api.id}/*/${local.method}${aws_api_gateway_resource.proxy.path}"
 
-  #   # source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
+  #source_arn    = "arn:aws:execute-api:${local.region}:${local.account_id}:${aws_api_gateway_rest_api.api.id}/*/${local.method}${aws_api_gateway_resource.proxy.path}"
+
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
   depends_on = ["aws_api_gateway_rest_api.api", "aws_api_gateway_resource.proxy"]
 }
