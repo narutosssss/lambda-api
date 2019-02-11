f92c7de08f289d4c949e97ff94f07aff5b20a3a3
Kritsada Sirirangsi <kritsada.s@orisma.com>
2019-02-11 16:28:50 +0700

    no message

--- modules/api/main.tf
+++ modules/api/main.tf
@@ -16,7 +16,7 @@ resource "aws_api_gateway_rest_api" "api" {
 resource "aws_api_gateway_resource" "proxy" {
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
   parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
-  path_part   = "${local.resource_name}"
+  path_part   = ""
 }
 
 resource "aws_api_gateway_method" "request_method" {
