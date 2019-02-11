c88df73c05049d78def8718dac0f1d8c6013cf0d
Kritsada Sirirangsi <kritsada.s@orisma.com>
2019-02-11 17:57:25 +0700

    no message

--- modules/lambda/output.tf
+++ modules/lambda/output.tf
@@ -6,6 +6,10 @@ output "arn" {
   value = "${aws_lambda_function.lambda.arn}"
 }
 
+output "invoke_arn" {
+  value = "${aws_lambda_function.lambda.invoke_arn}"
+}
+
 output "version" {
   value = "${aws_lambda_function.lambda.version}"
 }
